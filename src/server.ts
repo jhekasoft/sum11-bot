85e3253e86caae39a11ff33ad39368c1f3979dda
Eugene Efremov <jhekasoft@gmail.com>
2023-05-24 22:07:12 +0300

    Change chat model to the ChatGPT-3.5

--- src/server.ts
+++ src/server.ts
@@ -9,15 +9,25 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+bot.api.setMyCommands([
+  // { command: "start", description: "Start the bot" },
+  { command: "sum", description: "Посилання на СУМ-11" },
+]);
+
+bot.command("sum", (ctx) => {
+  if (!ctx.match) {
+    return ctx.reply("Додайте українське слово")
+  }
+  ctx.reply(`http://sum.in.ua/?swrd=${ctx.match}`)
+});
+
 // Reply to any message with OpenAI message
-bot.on("message", async (ctx) => {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: ctx.update.message.text,
-    temperature: 1,
-    max_tokens: 1000,
+bot.on("message:text", async (ctx) => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: ctx.msg.text}],
   });
-  const replyMessage = completion.data.choices[0].text;
+  const replyMessage = completion.data.choices[0].message.content;
   ctx.reply(replyMessage)
 });
 
